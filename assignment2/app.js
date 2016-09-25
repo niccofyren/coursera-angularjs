77dcaab2a73eb303364f29d97f1fc3c8ed7395e5
Nicolai Tellefsen <niccofyren@gmail.com>
2016-09-25 17:17:55 +0200

    Add Controller as syntax

--- assignment2/app.js
+++ assignment2/app.js
@@ -7,32 +7,36 @@ angular.module('ShoppingListCheckOff', [])
 	.controller('NewItemController', NewItemController)
 	.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-ToBuyShoppingController.$inject = ['$scope', 'ShoppingListCheckOffService'];
-function ToBuyShoppingController($scope, ShoppingListCheckOffService) {
-	$scope.items = ShoppingListCheckOffService.toBuy;
+ToBuyShoppingController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyShoppingController(ShoppingListCheckOffService) {
+	var toBuyCtrl = this;
 
-	$scope.buyItem = function(index) {
+	toBuyCtrl.items = ShoppingListCheckOffService.toBuy;
+	toBuyCtrl.buyItem = function(index) {
 		ShoppingListCheckOffService.buyItem(index);
 	};
 }
 
-AlreadyBoughtShoppingController.$inject = ['$scope', 'ShoppingListCheckOffService'];
-function AlreadyBoughtShoppingController($scope, ShoppingListCheckOffService) {
-	$scope.items = ShoppingListCheckOffService.bought;
+AlreadyBoughtShoppingController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtShoppingController(ShoppingListCheckOffService) {
+	var boughtCtrl = this;
 
-	$scope.returnItem = function(index) {
+	boughtCtrl.items = ShoppingListCheckOffService.bought;
+	boughtCtrl.returnItem = function(index) {
 		ShoppingListCheckOffService.returnItem(index);
 	};
 }
 
-NewItemController.$inject = ['$scope', 'ShoppingListCheckOffService'];
-function NewItemController($scope, ShoppingListCheckOffService) {
-	$scope.name = '';
-	$scope.quantity = 1;
-	$scope.addItem = function() {
-		ShoppingListCheckOffService.addItem($scope.name, $scope.quantity);
-		$scope.name = '';
-		$scope.quantity = 1;
+NewItemController.$inject = ['ShoppingListCheckOffService'];
+function NewItemController(ShoppingListCheckOffService) {
+	var newItemCtrl = this;
+
+	newItemCtrl.name = '';
+	newItemCtrl.quantity = 1;
+	newItemCtrl.addItem = function() {
+		ShoppingListCheckOffService.addItem(newItemCtrl.name, newItemCtrl.quantity);
+		newItemCtrl.name = '';
+		newItemCtrl.quantity = 1;
 	};
 }
 
